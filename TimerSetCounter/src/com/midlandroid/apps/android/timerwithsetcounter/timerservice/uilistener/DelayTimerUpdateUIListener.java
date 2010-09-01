cda0df4857c148dc4c07d893832a285669e04e4e
Jason Del Ponte <info@jasondelponte.com>
2010-09-01 03:08:32 +0000

    part of the change refactoring the project

--- TimerSetCounter/src/com/midlandroid/apps/android/timerwithsetcounter/timerservice/uilistener/DelayTimerUpdateUIListener.java
+++
@@ -1,5 +0,0 @@
-package com.midlandroid.apps.android.timerwithsetcounter.timerservice.uilistener;
-
-public interface DelayTimerUpdateUIListener {
-	public void updateDelayTimerUI(final long time);
-}
