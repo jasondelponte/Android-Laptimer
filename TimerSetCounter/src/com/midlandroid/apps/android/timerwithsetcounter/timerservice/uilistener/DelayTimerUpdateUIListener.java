61a58e4417087d64c255cc8e1ca8f00dea989ac7
Jason Del Ponte <info@jasondelponte.com>
2010-09-01 04:09:47 +0000

    Cleaning up the project to a usable state

---
+++ TimerSetCounter/src/com/midlandroid/apps/android/timerwithsetcounter/timerservice/uilistener/DelayTimerUpdateUIListener.java
@@ -0,0 +1,5 @@
+package com.midlandroid.apps.android.timerwithsetcounter.timerservice.uilistener;
+
+public interface DelayTimerUpdateUIListener {
+	public void updateDelayTimerUI(final long time);
+}
